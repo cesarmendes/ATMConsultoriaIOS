be4243c96264da258bc5d747197a5be9cad9fec7
Cesar Mendes da Silva <cesarmendesdasilva@mac-mini.local>
2017-10-02 21:20:53 -0300

    Initial Commit

---
+++ ATMConsultoriaIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ATMConsultoriaIOS
+//
+//  Created by Cesar Mendes da Silva on 02/10/2017.
+//  Copyright © 2017 Cesar Mendes da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
